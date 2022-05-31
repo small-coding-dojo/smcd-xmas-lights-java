0b77d7e467c0de43403ac8083aabb875ef875416
Stefan Boos <kontakt@boos.systems>
2022-05-31 17:48:14 +0200

    refactor: single layer of abstraction for main()
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Tugbay Atilla <tugbayatilla@gmail.com>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Falk Kühnel <falk@kuehnel.eu>

--- src/main/java/com/github/small_coding_dojo/xmas_lights/ChristmasLights.java
+++ src/main/java/com/github/small_coding_dojo/xmas_lights/ChristmasLights.java
@@ -2,22 +2,27 @@ package com.github.small_coding_dojo.xmas_lights;
 
 public class ChristmasLights {
     public static void main(String[] args) {
-        LightMatrix christmasLights = new LightMatrix();
-
-        christmasLights.turnOn(new Area(887, 9, 959, 629));
-        christmasLights.turnOn(new Area(454, 398, 844, 448));
-
-        christmasLights.turnOff(new Area(539, 243, 559, 965));
-        christmasLights.turnOff(new Area(370, 819, 676, 868));
-        christmasLights.turnOff(new Area(145, 40, 370, 997));
-        christmasLights.turnOff(new Area(301, 3, 808, 453));
-
-        christmasLights.turnOn(new Area(351, 678, 951, 908));
-
-        christmasLights.toggle(new Area(720, 196, 897, 994));
-        christmasLights.toggle(new Area(831, 394, 904, 860));
-
+        LightMatrix christmasLights = performKataOperations();
         renderOutput(christmasLights);
+    }
+
+    private static LightMatrix performKataOperations() {
+        LightMatrix result = new LightMatrix();
+
+        result.turnOn(new Area(887, 9, 959, 629));
+        result.turnOn(new Area(454, 398, 844, 448));
+
+        result.turnOff(new Area(539, 243, 559, 965));
+        result.turnOff(new Area(370, 819, 676, 868));
+        result.turnOff(new Area(145, 40, 370, 997));
+        result.turnOff(new Area(301, 3, 808, 453));
+
+        result.turnOn(new Area(351, 678, 951, 908));
+
+        result.toggle(new Area(720, 196, 897, 994));
+        result.toggle(new Area(831, 394, 904, 860));
+
+        return result;
     }
 
     @SuppressWarnings("java:S106") // Suppress sonar warning for logging via system.out.println
